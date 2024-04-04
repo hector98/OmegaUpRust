1b8436da36f40da2edbe8078472556274f8d076c
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2024-04-04 15:09:56 -0600

    pb_cuadrado, resuelto al 60%, falta optimizar uso de memoria

--- pb_cuadrado/src/main.rs
+++ pb_cuadrado/src/main.rs
@@ -7,23 +7,18 @@ fn main() {
         .read_line(&mut n)
         .expect("Failed to read line");
     let n: i16 = n.trim().parse().unwrap();
-    let mut m_original: Vec<String> = Vec::new();
+    let mut m_original = vec![vec![]; n as usize];
 
-    for _ in 1..=n {
+    for i in 0..n {
         let mut line = String::new();
         io::stdin()
             .read_line(&mut line)
             .expect("Failed to read line");
+        line = line.replace("\n", "");
+        //let line: Vec<String> = line.split(' ').map(|s| s.to_string()).collect();
 
-        
-        //let line: Vec<i16> = line
-        //  .trim()
-        //  .split(' ')
-        //  .map(|s| s.parse().unwrap())
-        //  .collect();
-        //println!("{}", line[0..line.len()-1].to_string());
-
-        m_original.push(line[0..line.len()-1].to_string());
+        m_original[i as usize] = line.split(' ').map(|s| s.to_string()).collect();
+        drop(line);
     }
 
     //println!("{:?}", m_original);
@@ -31,17 +26,30 @@ fn main() {
     let mut m_giro = String::from("");
     let mut i: i16 = n - 1;
     let mut j: i16 = 0;
-    while i >= 0 {
-        let aux_str = m_original[i as usize].chars().rev().collect::<String>();
-        //m_giro.push_str(aux_str);
-
-        if j != 1 {
-            m_giro.push_str("\n");
-        }
+    loop {
+        //println!("{}", m_original[i as usize][j as usize]);
+        let aux_str = &m_original[i as usize][j as usize].as_str();
+        m_giro += format!("{} ", &aux_str).as_str();
         
-        i -= 1;
-        j += 1;
+        if i == 0 {
+            j += 1;
+            m_giro.push_str("\n");
+            if j == n {
+                //i = 0;
+                drop(aux_str);
+                break;
+            } else {
+                i = n - 1;
+            }
+        } else {
+            i -= 1;   
+        }
+
     }
 
+    i = 0;
+    j = 0;
+    drop(m_original);
     println!("{}", m_giro);
+    drop(m_giro);
 }
