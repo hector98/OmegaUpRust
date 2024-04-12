44aa4fd487eafdbb10aa97b3ba37f66b402f92ce
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2024-04-12 09:02:11 -0600

    resuleto ordenando por distancia al origen, solo que excede el tiempo

--- ordenando_por_distancia_al_origen/src/main.rs
+++ ordenando_por_distancia_al_origen/src/main.rs
@@ -1,5 +1,6 @@
 // https://omegaup.com/arena/problem/Ordenando-por-distancia-al-orige/
 use std::io;
+use std::convert::TryInto;
 
 fn main() {
     let mut n = String::new();
@@ -7,7 +8,7 @@ fn main() {
         .read_line(&mut n)
         .expect("Failed to read line");
     let n: i32 = n.trim().parse().expect("Please type a number");
-    let mut array = vec![vec![::f32; 3]; n as usize];
+    let mut array = vec![vec![0.0; 3]; n as usize];
 
     for _ in 0..n {
         let mut x_y = String::new();
@@ -20,19 +21,43 @@ fn main() {
         let y: f32 = y.trim().parse().expect("Please type a number");
         let distancia: f32 = (x.powf(2.0) + y.powf(2.0)).sqrt();
 
-        let mut i = n - 1;
-        loop {
-            if distancia < array[i][2] {
-                let aux = array[i];
-                array[i] = [x, y, distancia];
-            } else if distancia == array[i][2] {
-                if array[i][0] < x {
-                } else if array[i][1] < y {
-                } else {
-                }
-            }
-        }
+        let mut i: usize = (n - 1).try_into().unwrap();
+        let point = [x, y, distancia].to_vec();
+        array[i] = point;
 
-        println!("{}", distancia);
+        loop {
+            let aux: &Vec<f32> = &array[i].to_vec();
+
+            if array[i][2] < array[i - 1][2] || array[i - 1][2] == 0.0 {
+                let aux = &array[i].to_vec();   
+                array[i] = array[i - 1].to_vec();
+                array[i - 1] = aux.to_vec();
+            } else if array[i][2] == array[i - 1][2] {
+                if array[i][0] < array[i - 1][0] {
+                    let aux = &array[i].to_vec();
+                    array[i] = array[i - 1].to_vec();
+                    array[i - 1] = aux.to_vec();
+                } else if array[i][0] == array[i - 1][0] {
+                    if array[i][1] < array[i - 1][1] {
+                        let aux = &array[i].to_vec();
+                        array[i] = array[i - 1].to_vec();
+                        array[i - 1] = aux.to_vec();
+                    }
+                }
+            } else {
+                break;
+            }
+
+            i -= 1;
+
+            if i == 0 {
+                break;
+            }
+
+        }
+    }
+
+    for i in 0..n {
+        println!("{:.0} {:.0}", array[i as usize][0], array[i as usize][1]);
     }
 }
